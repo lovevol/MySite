8af03e7a5c9b4c03afaaca7d3eaa3862cb029d39
lh <935686942@qq.com>
2018-03-02 12:13:16 +0800

    rabbitmq集成

--- src/main/java/rabbitmq/Producer.java
+++ src/main/java/rabbitmq/Producer.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class Producer {
     private static final String EXCHANGE_KEY = "mySite_exchange";
-    private static final String QUWUE_KEY = "test_queue";
+    private static final String QUEUE_KEY = "test_queue";
     @Autowired
     AmqpTemplate amqpTemplate;
      public void sendMessage(Object object){
         // convertAndSend 将Java对象转换为消息发送至匹配key的交换机中Exchange
-         amqpTemplate.convertAndSend(EXCHANGE_KEY, QUWUE_KEY, object);
+         amqpTemplate.convertAndSend(EXCHANGE_KEY, QUEUE_KEY, object);
      }
 }
